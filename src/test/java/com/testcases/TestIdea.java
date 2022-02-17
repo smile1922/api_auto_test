a9a9894392417593bf34f5ce4493f1f7aab82785
“songxudan” <songxudan@koolearn.com>
2022-02-17 10:08:40 +0800

    新增TestIdea.class 文件

---
+++ src/test/java/com/testcases/TestIdea.java
@@ -0,0 +1,10 @@
+package com.testcases;
+
+import org.testng.annotations.Test;
+
+public class TestIdea {
+    @Test
+    public void testIdea(){
+        System.out.println("新增class文件通过idea上传git");
+    }
+}
